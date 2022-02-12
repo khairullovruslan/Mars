1ffa2a7368497a7357358e06117ab410168841b7
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-12 14:09:16 +0300

    second

--- server.py
+++ server.py
@@ -3,45 +3,75 @@ from flask import Flask, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
-def title():
-    return "Миссия Колонизация Марса"
+@app.route("/")
+def index():
+    return "<h1>Миссия Колонизация Марса!</h1>"
 
 
-@app.route('/index')
-def slogan():
-    return "И на Марсе будут яблони цвести!"
+@app.route("/index")
+def index2():
+    return "<h1>И на Марсе будут яблони цвести!</h1>"
 
 
-@app.route('/promotion')
-def advertising():
-    return '''Человечество вырастает из детства.</br>
-
-Человечеству мала одна планета.</br>
-
-Мы сделаем обитаемыми безжизненные пока планеты.</br>
-
-И начнем с Марса!</br>
-Присоединяйся!</br>'''
+@app.route("/promotion")
+def promotion():
+    return f"""Человечество вырастает из детства.<br>
+Человечеству мала одна планета.<br>
+Мы сделаем обитаемыми безжизненные пока планеты.<br>
+И начнем с Марса!<br>
+Присоединяйся!"""
 
 
-@app.route('/image_mars')
+@app.route("/image_mars")
 def image_mars():
     return f"""<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <img src="{url_for('static', filename='img/mars.jpg')}" alt="здесь должна была быть картинка, но не нашлась">
-                    <br>
-                    <p1>Вот она какая, красная планета!</p1>
-                  </body>
-                </html>"""
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <title>Привет, Марс!</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="{url_for('static', filename='img/mars.jpg')}" alt="здесь должна была быть картинка, но не нашлась">
+                            <div>Вот она какая, красная планета</div>
+                          </body>
+                        </html>"""
+
+
+@app.route("/promotion_image")
+def promotion_image():
+    return f"""<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                            <title>Колонизация</title>
+                          </head>
+                          <body>
+                            <h1>Жди нас, Марс!</h1>
+                            <img src="{url_for('static', filename='img/mars.jpg')}" alt="здесь должна была быть картинка, но не нашлась">
+                            <div class="alert alert-dark" role="alert">
+                                Человечество вырастает из детства.
+                            </div>
+                            <div class="alert alert-success" role="alert">
+                                Человечеству мала одна планета.
+                            </div>
+                            <div class="alert alert-secondary" role="alert">
+                                Мы сделаем обитаемыми безжизненные пока планеты.
+                            </div>
+                            <div class="alert alert-warning" role="alert">
+                                И начнем с Марса!
+                            </div>
+                            <div class="alert alert-danger" role="alert">
+                               Присоединяйся!
+                            </div>
+                          </body>
+                        </html>"""
 
 
 if __name__ == '__main__':
-    app.run(port=8081, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
