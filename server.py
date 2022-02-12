84c9a758350e0b341fde2b7842cd41b13f7e6219
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-12 15:53:40 +0300

    # 5

--- server.py
+++ server.py
@@ -73,9 +73,34 @@ def promotion_image():
                         </html>"""
 
 
-@app.route("/astronaut_selection")
-def astronaut_selection():
-    pass
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                               <html lang="en">
+                                 <head>
+                                   <meta charset="utf-8">
+                                   <link rel="stylesheet" 
+                               href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                               integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                               crossorigin="anonymous">
+                                   <title>Результаты</title>
+                                 </head>
+                                 <body>
+                                   <h1>Результаты отбора</h1>
+                                   <h2>Претендента на участие в миссии {nickname};</h2>
+                                   <div class="alert alert-success" role="alert">
+                                      Поздравляем!Ваш рейтинг после {level} отбора;
+                                   </div>
+                                   
+                                   <div class="alert alert-secondary" role="alert">
+                                      составляет {rating}!;
+                                   </div>
+                                   <div class="alert alert-warning" role="alert">
+                                      Желаем удачи!
+                                   </div>
+                                  
+                                 </body>
+                               </html>"""
 
 
 @app.route('/choice/<planet_name>')
@@ -107,6 +132,7 @@ def choice(planet_name):
                                 </div>
                               </body>
                             </html>"""
+
 
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
