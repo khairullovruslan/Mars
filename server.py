afc59e1931ed4ede5483ec829998c8f1e0f0a222
khairullovruslan <khairullov.ruslan@gmail.com>
2022-02-12 15:37:11 +0300

    # 4

--- server.py
+++ server.py
@@ -78,6 +78,36 @@ def astronaut_selection():
     pass
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <link rel="stylesheet" 
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                            crossorigin="anonymous">
+                                <title>Варианты выбора</title>
+                              </head>
+                              <body>
+                                <h1>Мое предложение: {planet_name}</h1>
+                                <h2>Эта планета близка к земле;</h2>
+                                <div class="alert alert-success" role="alert">
+                                    На ней много необходимых ресурсов;
+                                </div>
+                                <div class="alert alert-secondary" role="alert">
+                                    На ней есть вода и атмосфера;
+                                </div>
+                                <div class="alert alert-warning" role="alert">
+                                    На ней есть небольшое магнитное поле;
+                                </div>
+                                <div class="alert alert-danger" role="alert">
+                                    Наконец, она просто красива!
+                                </div>
+                              </body>
+                            </html>"""
+
 @app.route('/form_sample', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
